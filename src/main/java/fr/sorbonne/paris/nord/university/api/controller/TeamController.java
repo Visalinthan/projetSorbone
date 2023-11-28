9de49ba43375848ecff0412ac7b8975ad807bd93
zhang <jinz0920@yahoo.fr>
2023-11-28 18:26:07 +0100

    first commit

---
+++ src/main/java/fr/sorbonne/paris/nord/university/api/controller/TeamController.java
@@ -0,0 +1,13 @@
+package fr.sorbonne.paris.nord.university.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+	@GetMapping("/hello")
+	 public String getTeams() {
+	 return "Hello World";
+	 }
+
+}
