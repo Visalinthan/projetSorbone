f704e4f448ddcaa08606ceb066d0605e6be0c2a4
Visalinthan <visalinthan@gmail.com>
2023-11-29 17:32:57 +0100

    step dto controller

---
+++ src/main/java/fr/sorbonne/paris/nord/university/api/dto/TeamDto.java
@@ -0,0 +1,7 @@
+package fr.sorbonne.paris.nord.university.api.dto;
+
+public class TeamDto {
+
+    private String name;
+    private String slogan;
+}
---
+++ src/main/java/fr/sorbonne/paris/nord/university/api/service/TeamService.java
@@ -0,0 +1,37 @@
+package fr.sorbonne.paris.nord.university.api.service;
+
+
+import fr.sorbonne.paris.nord.university.api.entity.TeamEntity;
+import fr.sorbonne.paris.nord.university.api.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TeamService  {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    public TeamService() {
+        this.teamRepository= teamRepository;
+    }
+
+    public List<TeamEntity> getAllTeam(){
+        return this.teamRepository.findAll();
+    }
+
+    public Optional<TeamEntity> getTeamById(Long id){
+        return this.teamRepository.findById(id);
+    }
+
+    public void saveTeam(TeamEntity team){
+          this.teamRepository.save(team);
+    }
+
+    public void deleteTeam(Long id){
+        this.teamRepository.deleteById(id);
+    }
+}
